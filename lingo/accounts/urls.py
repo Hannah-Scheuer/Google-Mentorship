954eb391c435ee3ca92398a957ea13c7a6dbf2dd
Stephanie <genericusername00000000@gmail.com>
2021-05-07 15:14:48 -0400

    add authentication server logic

--- lingo/accounts/urls.py
+++ lingo/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import UserView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', UserView.as_view()),
+    path('register/', UserView.as_view()),
+    path('login/', obtain_auth_token, name='auth_token'),
 ]
