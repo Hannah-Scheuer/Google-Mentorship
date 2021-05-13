722a6a8820b2094595d14223f1bc254985b72e89
Hannah-Scheuer <hscheuer20@gmail.com>
2021-05-13 14:01:52 -0400

    created the react app boilerplate

---
+++ lingo-frontend/src/app/components/AuthForm/index.tsx
@@ -0,0 +1,19 @@
+/**
+ *
+ * AuthForm
+ *
+ */
+import * as React from 'react';
+
+interface Props {}
+
+export function AuthForm(props: Props) {
+  return (
+    <div>
+      This will be a form
+      <form>
+        <input />
+      </form>
+    </div>
+  );
+}
