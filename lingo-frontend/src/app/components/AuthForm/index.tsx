a878fb50ee8d13bc59f1a033b524cea48a4b6fd7
Hannah-Scheuer <hscheuer20@gmail.com>
2021-05-17 16:35:03 -0400

    changed the html of the authform

--- lingo-frontend/src/app/components/AuthForm/index.tsx
+++ lingo-frontend/src/app/components/AuthForm/index.tsx
@@ -10,9 +10,53 @@ interface Props {}
 export function AuthForm(props: Props) {
   return (
     <div>
-      This will be a form
       <form>
-        <input />
+        <p> username </p>
+        <input
+          type="text"
+          placeholder="username"
+          //value={formValues.username}
+          name="username"
+          //onChange={handleInputChange}
+        />
+        <p> password </p>
+        <input
+          type="password"
+          //value={formValues.password1}
+          name="password1"
+          placeholder="password1"
+          //onChange={handleInputChange}
+        />
+        <p> confirm password </p>
+        <input
+          type="password"
+          //value={formValues.password2}
+          name="password2"
+          placeholder="password2"
+          //onChange={handleInputChange}
+        />
+        <p> email </p>
+        <input
+          type="email"
+          //value={formValues.email}
+          name="email"
+          placeholder="email"
+          //onChange={handleInputChange}
+        />
+        <p> languages you know </p>
+        <select name="languages_known">
+          <option>Spanish</option>
+          <option>English</option>
+          <option>Mandarin</option>
+        </select>
+        <p> languages you would like to learn </p>
+        <select name="languages_to_learn">
+          <option>Spanish</option>
+          <option>English</option>
+          <option>Mandarin</option>
+        </select>
+        <p> submit here! </p>
+        <input type="submit" value="submit" />
       </form>
     </div>
   );
