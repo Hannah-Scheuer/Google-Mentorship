fb9ae792c8861000b7b080950b6a54587f05d4d8
Stephanie <genericusername00000000@gmail.com>
2021-05-19 21:42:18 -0400

    remove cors

--- lingo/lingo/settings.py
+++ lingo/lingo/settings.py
@@ -41,14 +41,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'rest_framework.authtoken',
-    'corsheaders',
     'frontend',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -138,10 +136,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "../lingo-frontend/build/static")
