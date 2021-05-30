59281af55bbf1706e35cfaa13f122d848e4524a3
Stephanie <genericusername00000000@gmail.com>
2021-05-30 19:21:19 -0400

    add chatrooms

--- lingo/accounts/serializers.py
+++ lingo/accounts/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Room
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('pk', 'username', 'email', 'password', 'languages_known', 'languages_to_learn')
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    #users = UserSerializer(many=True)
+
+    class Meta:
+        model = Room
+        fields = ('url', 'users',)
