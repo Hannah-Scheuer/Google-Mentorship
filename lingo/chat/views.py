44d00a2b792323a9e39aeca620ba02b5622fef2d
Stephanie <genericusername00000000@gmail.com>
2021-06-03 13:37:31 -0400

    add authentication for websocket

--- lingo/chat/views.py
+++ lingo/chat/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, 'chat/index.html')
+from rest_framework.authtoken.models import Token
+from accounts.models import Room
+from django.http import HttpResponseNotFound, HttpResponseForbidden
+from django.core.exceptions import ValidationError
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name': room_name
-    })
+    try:
+        room = Room.objects.get(url=room_name)
+    except (ValidationError, Room.DoesNotExist):
+        return HttpResponseNotFound()
+
+    try:
+        token = request.COOKIES.get('token')
+        user = Token.objects.get(key=token).user
+        if user in room.users.all():
+            return render(request, 'chat/room.html', {
+                'room_name': room_name,
+                'user': user.username,
+                'token': token,
+            })
+        return HttpResponseForbidden() 
+    except:
+        return HttpResponseForbidden() 
